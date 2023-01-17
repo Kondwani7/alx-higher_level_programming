cab74b7a7b55eabcb9307ff05231694afd4a0e35
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-01-17 09:12:06 +0200

    q4 initial sub

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 // A script that prints the title of a Star Wars movie where the episode num matches a given int
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
